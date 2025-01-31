7e46361b6618bb66553ff5f755b763991e0e6b47
Jessica Batbayar <jbatbayar@ucsd.edu>
2025-01-31 00:57:31 -0800

    test json

--- global.js
+++ global.js
@@ -87,3 +87,21 @@ form?.addEventListener("submit", function (event) {
   // console.log(url);
   location.href = url;
 });
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+      }
+      
+      const data = await response.json();
+      return data; 
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
