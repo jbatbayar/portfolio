82328439c950d233c08d6e9d86d62c94d4cdc553
Jessica Batbayar <jbatbayar@ucsd.edu>
2025-02-12 01:00:51 -0800

    stats page added

--- global.js
+++ global.js
@@ -13,6 +13,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: "resume/cv.html", title: "Resume" },
   { url: "contact/", title: "Contact" },
+  { url: "meta/", title: "Meta" },
   { url: "https://github.com/jbatbayar", title: "Github Profile"},
 ];
 
---
+++ meta/main.js
@@ -0,0 +1,72 @@
+let data = [];
+
+
+// let commits = d3.groups(data, (d) => d.commit);
+// console.log(commits);
+let commits = [];
+
+function processCommits() {
+    commits = d3
+      .groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+        let ret = {
+          id: commit,
+          url: 'https://github.com/jbatbayar/portfolio/commit/' + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
+  
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          configurable: false,
+          writable: false,
+          enumerable: false
+          // What other options do we need to set?
+          // Hint: look up configurable, writable, and enumerable
+          // Hide from console logs
+        });
+  
+        return ret;
+      });
+  }
+
+
+loadData();
+function displayStats() {
+    // Process commits first
+    processCommits();
+  
+    // Create the dl element
+    const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+  
+    // Add total LOC
+    dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+    dl.append('dd').text(data.length);
+  
+    // Add total commits
+    dl.append('dt').text('Total commits');
+    dl.append('dd').text(commits.length);
+  
+    // Add more stats as needed...
+  }
+async function loadData() {
+    data = await d3.csv('loc.csv', (row) => ({
+      ...row,
+      line: Number(row.line), // or just +row.line
+      depth: Number(row.depth),
+      length: Number(row.length),
+      date: new Date(row.date + 'T00:00' + row.timezone),
+      datetime: new Date(row.datetime),
+    }));
+    // console.log(data);
+    // processCommits();
+    // console.log(commits);
+    displayStats();
+}
